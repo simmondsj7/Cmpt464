fbef1d55073062b660bbf7098d668e522721a2e2
John <simmondsj7@mymacewan.ca>
2018-10-22 18:18:50 -0600

    checking to see if this will now push to github

--- Assignment1/UART_excercise2.c
+++ Assignment1/UART_excercise2.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <intrinsics.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define R 0x04
 #define G 0x02
@@ -18,7 +19,6 @@ do{ \
   P4DIR |= R+G+Y; \
   P4OUT |= R+G+Y; \
   P3SEL |= 0x30; \
-}while(0)
 
 struct Buffer{
   uint8_t size;
@@ -27,26 +27,39 @@ struct Buffer{
   uint8_t data[5];
 };
 
-struct Buffer ringbuf= {0};
+struct Buffer ringbuf= {.in=0, .out=0 , .size=0};
 
 
 
 void push(){
-    ringbuf.data[ringbuf.in] = U0RXBUF; 
-    ringbuf.size ++;
+///////////////////////
+if(ringbuf.size==5){
+   ringbuf.size = 0;
+   ringbuf.in=0;
+   ringbuf.out=0;
+   memset(ringbuf.data, 0, sizeof(ringbuf.data));
+   return;
+  };
+////////////////////////////////
+    ringbuf.data[ringbuf.in] = U0RXBUF;
+    ringbuf.size++;
     ringbuf.in = (ringbuf.in + 1) % 5;
 }
 
-void pop(struct Buffer* ringbuf){
-    U0TXBUF = ringbuf->data[ringbuf->out];
-    ringbuf->size --;
-    ringbuf->out = (ringbuf->out + 1) % 5;
+void pop(){
+///////////////////////
+if(ringbuf.size==0)
+return;
+////////////////////////////////
+    U0TXBUF = ringbuf.data[ringbuf.out];
+    ringbuf.size--;
+    ringbuf.out = (ringbuf.out + 1) % 5;
 }
 
 // TBCCR1-3 interrupt handler
 __attribute__((interrupt(TIMERB1_VECTOR))) void timer_handler()
 {
-	// Accessing TBIV automatically resets the hight pending interrupt flag 
+    // Accessing TBIV automatically resets the hight pending interrupt flag
   switch(TBIV)
     {
     case TBIV_TBIFG:
@@ -57,27 +70,27 @@ __attribute__((interrupt(TIMERB1_VECTOR))) void timer_handler()
 // receive interrupt handler
 __attribute__((interrupt(USART0RX_VECTOR))) void receive_handler()
 {
-  if(ringbuf->size == 5) {
+  if(ringbuf.size == 5) {
     return;
-  } else if(ringbuf->size == 0) {
-	// start the clock
-	TBCTL = TBSSEL_1 + CNTL_1 + MC_2 + TBIE + ID_3;
+  } else if(ringbuf.size == 0) {
+    // start the clock
+    TBCTL = TBSSEL_1 + CNTL_1 + MC_2 + TBIE + ID_3;
     };
-    push(ringbuf);
+    push();
 }
 
 // transmit interrupt handler
 __attribute__((interrupt(USART0TX_VECTOR))) void transmit_handler()
 {
-	pop(ringbuf); 
-	if(ringbuf->size == 0){
-	    IE1 &= ~UTXIE0; // if there is nothing in the buffer dont transmit anything	
-	}
+    pop();
+    if(ringbuf.size == 0){
+        IE1 &= ~UTXIE0; // if there is nothing in the buffer dont transmit anything   
+    }
 }
 
 // Notes for the timer
 // adjust word length also the divider 2^12 * 8 || 8 is the divider
-// enable interupt for tbifg 
+// enable interupt for tbifg
 
 int main()
 {
@@ -93,7 +106,7 @@ int main()
   U0CTL |= CHAR;
   // Select ACLK clock source
   U0TCTL |= I2CSSEL_1;
-  
+ 
   // Set baud rate to 2400
   U0BR1 = 0; U0BR0 = 0x0D; U0MCTL = 0x6B;
   // Enable Transmitting and receiving
@@ -103,5 +116,5 @@ int main()
   // Enable receiver interrupt
   IE1 |=   URXIE0;
 
-  LPM3;		
+  LPM3;       
 }
--- Assignment1/morse_code.c
+++ Assignment1/morse_code.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <intrinsics.h>
 #include "morse_code.h"
+#include <stdlib.h>
 
 //------------------------------------------
 // John Simmonds
@@ -12,27 +13,91 @@
 // Keyboard input of characters
 //-------------------------------------------
 
-struct Buffer* ringbuf= {0};
+struct Buffer ringbuf= {.in=0, .out=0 , .size=0};
 
 
 
-void push(struct Buffer* ringbuf){
-    ringbuf->data[ringbuf->in] = U0RXBUF; 
-    ringbuf->size ++;
-    ringbuf->in = (ringbuf->in + 1) % 64;
+void push(){
+///////////////////////
+if(ringbuf.size==32){
+   ringbuf.size = 0;
+   ringbuf.in=0;
+   ringbuf.out=0;
+   return;
+  };
+////////////////////////////////
+    ringbuf.data[ringbuf.in] = U0RXBUF; //issue here
+    ringbuf.size++;
+    ringbuf.in = (ringbuf.in + 1) % 32;
 }
 
-void pop(struct Buffer* ringbuf){
-    U0TXBUF = ringbuf->data[ringbuf->out];
-    ringbuf->size --;
-    ringbuf->out = (ringbuf->out + 1) % 64;
+void pop(){
+///////////////////////
+    if(ringbuf.size==0)
+        return;
+////////////////////////////////
+    char* code = check_character(ringbuf.data[rinfbuf.out], morse, letters);
+    U0TXBUF = code;
+    ringbuf.size--;
+    ringbuf.out = (ringbuf.out + 1) % 32;
 }
+
+// TBCCR1-3 interrupt handler
+__attribute__((interrupt(TIMERB1_VECTOR))) void timer_handler()
+{
+    // Accessing TBIV automatically resets the hight pending interrupt flag
+  switch(TBIV)
+    {
+    case TBIV_TBIFG:
+      IE1 |=   UTXIE0;
+    }
+}
+
+// receive interrupt handler
+__attribute__((interrupt(USART0RX_VECTOR))) void receive_handler()
+{
+  if(ringbuf.size == 32) { // if ringbuf size is full
+    return;
+  } else if(ringbuf.size == 0) {
+    // start the clock
+    TBCTL = TBSSEL_1 + CNTL_1 + MC_2 + TBIE + ID_3;
+    };
+    push();
+}
+
+// transmit interrupt handler
+__attribute__((interrupt(USART0TX_VECTOR))) void transmit_handler()
+{
+    pop();
+    if(ringbuf.size == 0){
+        IE1 &= ~UTXIE0; // if there is nothing in the buffer dont transmit anything   
+    }
+}
+
 
 int main() {
+  //Initialize structure
+  INIT_STRUCT;
   // disable the watchdog timer
   WDTCTL = WDTPW + WDTHOLD;
   // Enable maskable interrupts (See intrinsics.h)
   __eint();
+// serial pins to peripheral mode
+  INIT;
+  // Set SWRST bit
+  U0CTL |= SWRST;
+  // Char length 8 bits
+  U0CTL |= CHAR;
+  // Select ACLK clock source
+  U0TCTL |= I2CSSEL_1;
+ 
+  // Set baud rate to 2400
+  U0BR1 = 0; U0BR0 = 0x0D; U0MCTL = 0x6B;
+  // Enable Transmitting and receiving
+  ME1 |= URXE0 | UTXE0;
+  // Reset SWRST bit
+  U0CTL &= ~SWRST;
+  // Enable receiver interrupt
 
   LOW_POWER_MODE;
   // Initialize all leds to be off
