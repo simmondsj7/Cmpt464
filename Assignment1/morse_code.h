6e2d3e135683a277019851e4047e3ef1981366e0
John Simmonds <simmondsj7@mymacewan.ca>
2018-10-27 14:19:04 -0600

    syntax fixing

--- Assignment1/morse_code.h
+++ Assignment1/morse_code.h
@@ -52,10 +52,45 @@ typedef struct{
     int length;
 } MorseCode;
 
-MorseCode m[38] = {{.letter = 'a', .code = ".-", .length = 2},  
-{.letter = 'b', .code = "-...", .length = 4}, 
-{.letter = 'c', .code = "-.-.", .length = 4}, 
-{.letter = 'd', .code = "-..", .length = 3}};
+MorseCode m[38] = {{.letter = 'a', .code = ".-", .length = 2},  // 0
+{.letter = 'b', .code = "-...", .length = 4},                   // 1
+{.letter = 'c', .code = "-.-.", .length = 4},                   // 2
+{.letter = 'd', .code = "-..", .length = 3},                    // 3
+{.letter = 'e', .code = ".", .length = 1},                      // 4
+{.letter = 'f', .code = "..-.", .length = 4},                   // 5
+{.letter = 'g', .code = "--.", .length = 3},                    // 6
+{.letter = 'h', .code = "....", .length = 4},                   // 7
+{.letter = 'i', .code = "..", .length = 2},                     // 8
+{.letter = 'j', .code = ".---", .length = 4},                   // 9
+{.letter = 'k', .code = "-.-", .length = 3},                    // 10
+{.letter = 'l', .code = ".-..", .length = 4},                   // 11
+{.letter = 'm', .code = "--", .length = 2},                     // 12
+{.letter = 'n', .code = "-.", .length = 2},                     // 13
+{.letter = 'o', .code = "---", .length = 3},                    // 14
+{.letter = 'p', .code = ".--.", .length = 4},                   // 15
+{.letter = 'q', .code = "--.-", .length = 4},                   // 16
+{.letter = 'r', .code = ".-.", .length = 3},                    // 17
+{.letter = 's', .code = "...", .length = 3},                    // 18
+{.letter = 't', .code = "-", .length = 1},                      // 19
+{.letter = 'u', .code = "..-", .length = 3},                    // 20
+{.letter = 'v', .code = "...-", .length = 4},                   // 21
+{.letter = 'w', .code = ".--", .length = 3},                    // 22
+{.letter = 'x', .code = "-..-", .length = 4},                   // 23
+{.letter = 'y', .code = "-.--", .length = 4},                   // 24
+{.letter = 'z', .code = "--..", .length = 4},                   // 25
+{.letter = '1', .code = ".----", .length = 5},                  // 26
+{.letter = '2', .code = "..---", .length = 5},                  // 27
+{.letter = '3', .code = "...--", .length = 5},                  // 28
+{.letter = '4', .code = "....-", .length = 5},                  // 29
+{.letter = '5', .code = ".....", .length = 5},                  // 30
+{.letter = '6', .code = "-....", .length = 5},                  // 31
+{.letter = '7', .code = "--...", .length = 5},                  // 32
+{.letter = '8', .code = "---..", .length = 5},                  // 33
+{.letter = '9', .code = "----.", .length = 5},                  // 34
+{.letter = '0', .code = "-----", .length = 5},                  // 35
+{.letter = '.', .code = ".-.-.-", .length = 6},                 // 36 - Full stop
+{.letter = '@', .code = "...-.-", .length = 6},                 // 37 - End of Work
+{.letter = '~', .code = "-.-.-", .length = 3}};                 // 38 - Starting Signal
 
 MorseCode morse[s];
 Buffer ringbuf= {.in=0, .out=0, .size=0};
